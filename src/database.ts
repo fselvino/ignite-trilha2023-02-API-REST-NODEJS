bad56cadd46abea103d22fa11aeb0f274ba12f53
root <root@Servidor.localdomain>
2023-11-28 19:09:59 -0300

    Variavel de ambiente

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,17 @@
+import 'dotenv/config'
 import { knex as setupKnex, Knex } from 'knex'
+
+//console.log(process.env) //verifica o valor das variaveis de ambiente
+
+//if temporario somente para não dar erro
+if(!process.env.DATABASE_URL){
+  throw new Error('DATABASE_URL env not found.')
+}
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './db/app.sqlite',
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
