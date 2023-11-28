7e0c0d95f6cc144bc478894664ac64e9e81ef63c
root <root@MGO16105633.correiosnet.int>
2023-11-28 14:19:27 -0300

    configurnado ESLint

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
-//principais metodos GET, POST, PUT, PATCH, DELETE
+// principais metodos GET, POST, PUT, PATCH, DELETE
 
-app.get('/hello', ()=> {
+app.get('/hello', () => {
   return 'Hello World '
 })
 
-app.listen({
-  port:3333
-}).then(
-  ()=>{
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
     console.log('HTTP Server Running!')
-  }
-)
+  })
