0431ee4db86f9edebe57d630b48b40efad2bc303
root <root@Servidor.localdomain>
2023-11-28 18:57:40 -0300

    Realizando queries com knex

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import fastify from 'fastify'
+import { randomUUID } from 'crypto'
 import { knex } from './database'
 
 const app = fastify()
@@ -6,9 +7,20 @@ const app = fastify()
 // principais metodos GET, POST, PUT, PATCH, DELETE
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*') // sqlite_schema é criada automaticamente
+  const transaction = await knex('transactions').insert({
+    id: randomUUID(),
+    title: 'Transação de teste',
+    amount:1000,
 
-  return tables
+  }).returning('*')
+  return transaction//retorna todas informações inseridas
+  
+})
+app.get('/transactions', async ()=> {
+  const transactions = await knex('transactions')
+  .where('amount', 100)//onde amount = 100 - primeira coluna campo sengundo valor
+  .select('*')
+  return transactions
 })
 
 app
