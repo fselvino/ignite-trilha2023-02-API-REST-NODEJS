65e7375f569646be478f5d432ec9be8e97c00043
root <root@MGO16105633.correiosnet.int>
2023-11-29 09:30:08 -0300

    Tratando env com Zod

--- src/database.ts
+++ src/database.ts
@@ -1,17 +1,12 @@
-import 'dotenv/config'
 import { knex as setupKnex, Knex } from 'knex'
+import { env } from './env'
 
 // console.log(process.env) //verifica o valor das variaveis de ambiente
-
-// if temporario somente para não dar erro
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL env not found.')
-}
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: process.env.DATABASE_URL,
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify'
 import { randomUUID } from 'crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
@@ -25,7 +26,7 @@ app.get('/transactions', async () => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running!')
